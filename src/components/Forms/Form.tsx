167df7750d646de51ae17aefa0732e884aa192af
samssjubair <samssjubair@gmail.com>
2023-10-03 01:00:11 +0600

    department list table and create api integration

--- src/components/Forms/Form.tsx
+++ src/components/Forms/Form.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ReactElement, ReactNode } from "react";
+import { ReactElement, ReactNode, useEffect } from "react";
 import { useForm, FormProvider, SubmitHandler } from "react-hook-form";
 
 type FormConfig = {
@@ -32,6 +32,8 @@ const Form = ({
     submitHandler(data);
     reset();
   };
+
+  useEffect(() => reset(defaultValues), [defaultValues, reset, methods]);
 
   return (
     <FormProvider {...methods}>
--- src/components/ui/UploadImage.tsx
+++ src/components/ui/UploadImage.tsx
@@ -4,6 +4,7 @@ import type { UploadChangeParam } from "antd/es/upload";
 import type { RcFile, UploadFile, UploadProps } from "antd/es/upload/interface";
 import Image from "next/image";
 import { useState } from "react";
+import { useFormContext } from "react-hook-form";
 
 const getBase64 = (img: RcFile, callback: (url: string) => void) => {
   const reader = new FileReader();
@@ -23,9 +24,14 @@ const beforeUpload = (file: RcFile) => {
   return isJpgOrPng && isLt2M;
 };
 
-const UploadImage = () => {
+type ImageUploadProps = {
+  name: string;
+};
+
+const UploadImage = ({ name }: ImageUploadProps) => {
   const [loading, setLoading] = useState(false);
   const [imageUrl, setImageUrl] = useState<string>();
+  const { setValue } = useFormContext();
 
   const handleChange: UploadProps["onChange"] = (
     info: UploadChangeParam<UploadFile>
@@ -36,6 +42,7 @@ const UploadImage = () => {
     }
     if (info.file.status === "done") {
       // Get this url from response in real world.
+      setValue(name, info.file.originFileObj);
       getBase64(info.file.originFileObj as RcFile, (url) => {
         setLoading(false);
         setImageUrl(url);
@@ -53,16 +60,22 @@ const UploadImage = () => {
   return (
     <>
       <Upload
-        name="avatar"
+        name={name}
         listType="picture-card"
         className="avatar-uploader"
         showUploadList={false}
-        action="https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188"
+        action="/api/file"
         beforeUpload={beforeUpload}
         onChange={handleChange}
       >
         {imageUrl ? (
-          <Image src={imageUrl} alt="avatar" style={{ width: "100%" }} />
+          <Image
+            src={imageUrl}
+            alt="avatar"
+            style={{ width: "100%" }}
+            width={100}
+            height={100}
+          />
         ) : (
           uploadButton
         )}
